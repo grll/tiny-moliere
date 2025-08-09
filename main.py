992d704eef5f2af6e657c409b82dd041f45cce4a
Guillaume Raille <guillaume.raille@gmail.com>
2025-08-09 13:40:16 +0200

    add a download function

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
+import httpx
+
+RAW_PDF_URLS = [
+    "https://www.ebooksgratuits.com/ebooksfrance/moliere-oeuvres_completes_1.pdf",
+    "https://www.ebooksgratuits.com/ebooksfrance/moliere-oeuvres_completes_2.pdf",
+]
+
+def download_pdf(url: str, output_path: str) -> None:
+    with httpx.stream("GET", url, follow_redirects=True) as response:
+        response.raise_for_status()  # ensure we got a 200 OK
+        with open(output_path, "wb") as f:
+            for chunk in response.iter_bytes():
+                f.write(chunk)
+
 def main():
+    # 1. fetch raw data if (not already fetched)
+
+    # fetch from
+    # 2. process data into clean tiny-shakespeare like format
+
+    # 3. save processed data to disk
     print("Hello from tiny-moliere!")
 
 
